10b57c4e87efeee8bb3b372492d1784fbe9d6dbc
Tejas Shroff <tejasshroff24@gmail.com>
2019-01-19 13:39:02 -0500

    Detecting tracking states

---
+++ Assets/App/Scripts/ImageTrackerConfig.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+using UnityEngine.UI;
+
+public class ImageTrackerConfig : MonoBehaviour, ITrackableEventHandler {
+
+	public TrackableBehaviour imageTracker;
+
+	public Text imageTrackerText;
+
+	// Use this for initialization
+	void Start () {
+
+		if (imageTracker) 
+		{
+			imageTracker.RegisterTrackableEventHandler(this);
+		}
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void OnTrackableStateChanged(
+
+		TrackableBehaviour.Status previousStatus,
+
+		TrackableBehaviour.Status newStatus)
+
+	{
+
+		if (newStatus == TrackableBehaviour.Status.DETECTED ||
+
+			newStatus == TrackableBehaviour.Status.TRACKED ||
+
+			newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+
+		{
+
+			// When target is found
+
+			imageTrackerText.text = "Cereal Found";
+
+		}
+
+		else
+
+		{
+
+			// When target is lost
+
+			imageTrackerText.text = "Cereal NOT Found";
+
+		}
+
+	}
+}
---
+++ Assets/App/Scripts/ImageTrackerManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+using UnityEngine.UI;
+
+public class ImageTrackerManager : MonoBehaviour, ITrackableEventHandler {
+
+	public TrackableBehaviour cereal1Tracker;
+
+	public Text imageTrackerText;
+
+	// Use this for initialization
+	void Start () {
+
+		if (cereal1Tracker) 
+		{
+			cereal1Tracker.RegisterTrackableEventHandler(this);
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnTrackableStateChanged(
+
+		TrackableBehaviour.Status previousStatus,
+
+		TrackableBehaviour.Status newStatus)
+
+	{
+
+		if (newStatus == TrackableBehaviour.Status.DETECTED ||
+
+			newStatus == TrackableBehaviour.Status.TRACKED ||
+
+			newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+
+		{
+
+			// When target is found
+
+			imageTrackerText.text = "Cereal Found";
+
+		}
+
+		else
+
+		{
+
+			// When target is lost
+
+			imageTrackerText.text = "Cereal NOT Found";
+
+		}
+
+	}
+
+
+}
